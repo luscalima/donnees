1f3fc281da05cb909b7c79feb060ed489c9a93dc
LuscaLima <lusca.lima@outlook.com>
2024-10-11 11:41:00 -0300

    fix: set correct path to login redirect

--- pages/login.vue
+++ pages/login.vue
@@ -2,7 +2,7 @@
 import { GithubLogoIcon } from '@radix-icons/vue'
 
 const { auth } = useSupabaseClient()
-const redirectTo = `${window.location.origin}/confirm`
+const redirectTo = `${window.location.origin}/confirm/`
 
 async function handleLogin(provider: 'github' | 'google') {
   auth.signInWithOAuth({
